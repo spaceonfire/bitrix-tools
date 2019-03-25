7c1d2e744d7d8fab692738fa961ab331275f2a0f
Constantine Karnaukhov <genteelknight@gmail.com>
2019-03-25 16:17:08 +0400

    feat(Common): add addBodyClass method

--- src/Common.php
+++ src/Common.php
@@ -19,4 +19,18 @@ class Common
 			}
 		}
 	}
+
+	/**
+	 * Add body class to property
+	 * @param array $classes an array of class names
+	 * @param string $propertyId custom property id, 'BodyClass' by default
+	 */
+	public static function addBodyClass(array $classes, string $propertyId = 'BodyClass'): void
+	{
+		global $APPLICATION;
+		$arBodyClass = explode(' ', $APPLICATION->GetPageProperty($propertyId, ''));
+		$arBodyClass = array_merge($arBodyClass, $classes);
+		$arBodyClass = array_unique(array_filter($arBodyClass));
+		$APPLICATION->SetPageProperty($propertyId, implode(' ', $arBodyClass));
+	}
 }
